9c951cb72bbd97c21099f912ad2f187c4ec06715
Tomara Petty <tomararuth@gmail.com>
2023-04-02 15:48:55 -0700

    ADD: initial files

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import pizzas from '../data/pizzas.json';
+import Pizza from './Pizza';
+import AppCSS from './App.module.css';
+import PizzaSVG from '../svg/pizza.svg'
+
+const App = () => {
+  return (
+    <div className={AppCSS.container}>
+      <ul>
+        {pizzas.map(pizza => {
+          return <Pizza key={pizza.id} pizza={pizza} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
